c2fe453848e4cb87fe400f253e4757212fedf98b
natsuokawai <kawai.natsuo@gmail.com>
2021-07-05 11:57:31 +0900

    Add .gitignore_global

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 
