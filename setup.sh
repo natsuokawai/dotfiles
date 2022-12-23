6da886fe2190a4f938887c9a96d10e46634d2a83
natsuokawai <kawai.natsuo@gmail.com>
2022-12-23 10:46:21 +0900

    Add Alacritty

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.config/nvim ~/.config/nvim
+ln -sf ~/dotfiles/.config/alacritty ~/.config/alacritty
